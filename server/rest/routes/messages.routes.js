ad97b9e81a8a243ebeedb26c9864cdb287470bb2
Den-programmer <denisgrigorov094@gmail.com>
2025-09-26 16:00:46 +0200

    Prepared project to interact with Graph QL, installed Graph QL

--- server/rest/routes/messages.routes.js
+++ server/rest/routes/messages.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import MessagesController from '../controllers/MessagesController.js'
-import { verifyToken } from '../middleware/verifyToken.js'
+import { verifyToken } from '../../middleware/verifyToken.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
